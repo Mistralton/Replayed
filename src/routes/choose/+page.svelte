b991ddacdfc9a3c3addfa820acd456cf7967ec2c
Kyle <leungkyle2@gmail.com>
2023-02-18 14:13:49 -0800

    choose

--- src/routes/choose/+page.svelte
+++ src/routes/choose/+page.svelte
@@ -1,5 +1,26 @@
-<div class="flex items-center">
-  <img class="p-4" src="Valorant.png" />
-  <img class="p-4" src="League.png" />
-  <img class="p-4" src="Overwatch 2.png" />
+<div class="items-center flex flex-col justify-evenly h-screen">
+	<div class="flex justify-evenly w-screen">
+		<div class="p-10 w-1/3 flex flex-col justify-center">
+			<input class="mx-auto" placeholder="Search User..." />
+			<div class="mx-auto pr-10 pt-2 flex">
+				<img src="fire.png" alt="fire" class="w-10" />
+				<button class="text-black bg-white border-2 border-stone-500 w-32">Trending</button>
+			</div>
+		</div>
+		<div class="p-10 w-1/3 flex flex-col">
+			<h1 class="p-2 text-center text-white border-white border-2 rounded-md mx-auto">
+				Replayed.gg
+			</h1>
+			<p class="text-white text-center">Choose the game you want to guess</p>
+		</div>
+		<div class="p-10 w-1/3 flex justify-center">
+			<div class="bg-white h-20 rounded-lg w-16 h-16">profile icon</div>
+		</div>
+		<div />
+	</div>
+	<div class="flex">
+		<img class="p-4" src="Valorant.png" />
+		<img class="p-4" src="League.png" />
+		<img class="p-4" src="Overwatch 2.png" />
+	</div>
 </div>
