0f61d82a0dbc4d08b26f1e5b99890daafdfc55f1
Wesley <60171023+wesngu28@users.noreply.github.com>
2023-02-19 03:51:11 -0800

    only a should underline on hover

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,5 +1,5 @@
-<div class="p-4 text-black text-center hover:underline bg-stone-600">
-  <a class="mb-4" href="mistraltonco@gmail.com">Contact Us Here</a>
+<div class="p-4 text-black text-center bg-stone-600">
+  <a class="mb-4 hover:underline" href="mistraltonco@gmail.com">Contact Us Here</a>
   <p class="my-4">Made for Bay Area Hacks using SvelteKit, Tailwind, Supabase, Chart.js, and TypeScript.</p>
   <p class="mb-4">Kyle Leung, Reagan Vu, Wesley Nguyen 2023</p>
   <p class="mb-4">Not affiliated with Blizzard or Riot, just some students having fun.</p>
