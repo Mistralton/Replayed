993ba66130d066c8415bc305df4e311420f61b39
Kyle <leungkyle2@gmail.com>
2023-02-20 08:50:46 -0800

    replay interface

--- src/lib/db_res.ts
+++ src/lib/db_res.ts
@@ -1,11 +1,11 @@
 export interface Replay {
+  id: number
   created_at: string
   game: string
   rank: string
-  usr: string
+  user: string
   guesses: Array<{rank: string, guesses: number}>,
   comments: Array<{date: string, text: string}>
-  tags: null,
   link: string,
   duel_winner: number,
   total_duels: number
