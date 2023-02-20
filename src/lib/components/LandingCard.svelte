fc0417d04c9b4347a67205228a598d7087c812dd
ReaganVu29 <reaganvu29@gmail.com>
2023-02-20 09:11:37 -0800

    Updated home page visuals

--- src/lib/components/LandingCard.svelte
+++ src/lib/components/LandingCard.svelte
@@ -6,13 +6,13 @@
 
 <div class="mt-36 gap-16 mb-20 px-4 w-full max-w-3xl flex items-center justify-between text-white">
   {#if !order}
-    <img width="450" src="https://images.unsplash.com/photo-1670272498380-eb330b61f3cd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80" />
+    <img width="500" src="clips.webp" />
   {/if}
   <div>
     <h2 class="text-2xl font-semibold">{title}</h2>
     <p class="mt-4">{text}</p>
   </div>
   {#if order}
-    <img width="450" src="https://images.unsplash.com/photo-1670272498380-eb330b61f3cd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80" />
+    <img width="450" src="rankshome.webp" />
   {/if}
 </div>
